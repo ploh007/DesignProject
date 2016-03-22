1af9b81f461b437934587930e005382b68df3ec2
Jordan Hatcher <jhatch12@gmail.com>
2016-03-22 19:05:19 -0400

    basic gesture detection working

--- Java/Comparator.java
+++ Java/Comparator.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.io.*;
 
 public class Comparator {
 
@@ -9,7 +10,32 @@ public class Comparator {
     this.threshold = threshold;
     sampleDatabase = new LinkedList<Sample>();
 
-    //load data from json files
+    try(FileReader fr = new FileReader("samples.json");
+        BufferedReader br = new BufferedReader(fr);) {
+
+      String line;
+
+      while((line = br.readLine()) != null) {
+        
+        String [] rawSample = line.split(":");
+        String gesture = rawSample[0];
+
+        String [] rawFft = rawSample[1].split(",");
+        double [] fft = new double[rawFft.length];
+
+        for(int i=0;i<rawFft.length;i++) {
+          fft[i] = Double.parseDouble(rawFft[i]);
+        }
+
+        Sample sample = new Sample(fft, gesture);
+        sampleDatabase.add(sample);
+        System.out.println("Added new sample: " + sample.getGesture());
+      }
+    }catch(FileNotFoundException e){
+      System.out.println("Encountered a FileNotFoundException while reading json file.");
+    }catch(IOException e) {
+      System.out.println("Encountered an IOException while reading json file.");
+    }
   }
 
   public String getGesture(double [] fft) {
